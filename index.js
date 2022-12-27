7a6cbc0646715efdd022faede501bc7d5551df37
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2022-12-27 23:32:26 +0200

    step 4

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const router = express.Router();
 const app = express()
 const port = 3000
 
+// start first rout
 app.get('/', (req, res) => {
   res.send('OK!')
 })
+// end first rout
 
 
 
-
-
+//start test route
 app.get("/test", (req, res) => {
   const response = {
     status: 200,
@@ -18,7 +19,10 @@ app.get("/test", (req, res) => {
   };
   res.send(response);
 });
+//end test route
 
+
+//start time route
 app.get("/time", (req, res) => {
   let date_ob = new Date();
 
@@ -33,6 +37,56 @@ app.get("/time", (req, res) => {
 
   res.send(response);
 });
+// end time route
+
+
+
+
+// start Hello route
+
+app.get("/hello/:id" , (req, res) => {
+  const id = req.params.id;
+
+  const response = {
+    status: 200,
+    message: "hello" , id,
+  };
+  res.send(response);
+});
+
+// end Hello route
+
+
+// start search route
+
+app.get('/search',(req,res) => {
+   const search = req.query.s;
+ 
+
+      if (typeof search != 'undefined') {
+          const response = {status:200, message:"ok", data: search};res.send(response);
+      }
+      else {const response = {status:500, error:true, message: "you have to provide a search"};
+      res.status(500);res.send(response);
+      }
+});
+
+// end search route
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
