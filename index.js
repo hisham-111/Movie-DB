2e236d97301624be2aacfba9c06d333dea9a9a79
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2022-12-28 19:43:44 +0200

    step 5

--- index.js
+++ index.js
@@ -80,6 +80,69 @@ app.get('/search',(req,res) => {
 
 
 
+// start add rout
+app.get('/movies/add', (req, res) => {
+  res.send('add')
+})
+// end add rout
+
+
+
+// start get rout
+app.get('/movies/get', (req, res) => {
+  res.send('get')
+})
+// end get rout
+
+
+
+// start edit rout
+app.get('/movies/edit', (req, res) => {
+  res.send('edit')
+})
+// end edit rout
+
+
+
+// start delete rout
+app.get('/movies/delete', (req, res) => {
+  res.send('delete')
+})
+// end delete rout
+
+
+const movies = [
+  { title: 'Jaws', year: 1975, rating: 8 },
+  { title: 'Avatar', year: 2009, rating: 7.8 },
+  { title: 'Brazil', year: 1985, rating: 8 },
+  { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+
+app.get("/movies/read", (req, res) => {
+  res.send({
+      status: 200,
+      data: movies,
+  });
+});
+
+
+
+
+
+
+  // res.send(Array.isArray(myApp));
+
+
+
+         
+
+
+
+
+
+
+
+
 
 
 
